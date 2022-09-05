c6f2a99d19416c60ad6cb9e48130edd7bfb454af
Zi Jian Yew <zijian.yew@hmgics.com>
2022-09-05 16:16:35 +0800

    Support non-root

--- README.md
+++ README.md
@@ -1,30 +1,54 @@
 # Idle Notifier
 
-This script notifies the user using SimplePush when the computer is idle.
+This script notifies the user using SimplePush when the computer is idle. 
+
+Note: This repo is for internal use only. No support will be provided, use at your own risk.
+
+## Pre-requisites
+
+Install [SimplePush](https://simplepush.io) on your phone. [[Android](https://play.google.com/store/apps/details?id=io.simplepush)] [[iOS](https://apps.apple.com/us/app/simplepush-notifications/id1569978086)]
+
+Take note of your API key. 
 
 ## Installation
-First install the python package:
-```bash
-git clone https://github.com/yewzijian/idle_notifier.git
-pip install --upgrade -e ./idle_notifier
-```
+
+1. First install the python package. Either
+   
+   * Clone and install in develop mode
+     
+     ```bash
+     git clone https://github.com/yewzijian/idle_notifier.git
+     pip install --upgrade -e ./idle_notifier
+     ```
+   
+   * or directly install from this git repository
+     
+     ```bash
+     pip install https+https://github.com/yewzijian/idle_notifier.git
+     ```
 
 Then run the setup tool:
+
 ```bash
 idle_notifier_setup
 ```
-Input the machine name and api key when prompted. Then enable the system service with:
-```bash
-sudo systemctl enable idle_notifier
-```
 
+Follow the instructions and enter the machine name and api key when prompted.
 
 ## Uninstall
-Run the uninstall tool:
-```bash
-idle_notifier_remove
-```
-Then optionally remove the python package
-```bash
-pip uninstall idle_notifier
-```
+
+1. Run the uninstall tool:
+   
+   ```bash
+   idle_notifier_remove
+   ```
+
+2. Then optionally remove the python package
+   
+   ```bash
+   pip uninstall idle_notifier
+   ```
+
+## Additional notes
+
+* By default, the script checks CPU/GPU usage every minute. If it falls below both thresholds of 10% CPU and GPU usage for over 30min, a notification will be sent. Currently there is no functionality to easily configure these parameters, but they can be edited manually in `config.json`.
--- src/idle_notifier/__init__.py
+++ src/idle_notifier/__init__.py
@@ -5,22 +5,31 @@ import sys
 import subprocess
 
 
-def idle_notifier_setup():
-    if os.geteuid() == 0:
-        run_setup()
-    else:
+def setup_wrapper(mode='install'):
+
+    if not os.path.exists('/etc/systemd/system'):
+        print('Error: Unable to find /etc/systemd/system folder. This tool is only for Linux systems. Terminating...')
+        exit(-1)
+
+    if os.geteuid() != 0:
         python_path = sys.executable
-        result = input('You need sudo to run this setup script. Run with sudo rights? [y/n]')
+        result = input('This script requires sudo rights to modify /etc/systemd. '
+                       'Alternatively, you can run without sudo rights and follow the '
+                       'displayed commands to modify it yourself. '
+                       'Run with sudo [y/n]: ')
         while result not in ['y', 'n']:
-            result = input('Invalid response. Run setup script with sudo? [y/n]')
+            result = input('Invalid response. Run setup script with sudo? [y/n] ')
         if result == 'y':
             subprocess.call(['sudo', python_path, *sys.argv])
             sys.exit()
-        else:
-            print('Terminating...')
+
+    if mode == 'install':
+        run_setup(has_sudo=os.geteuid() == 0)
+    elif mode == 'uninstall':
+        run_uninstall(has_sudo=os.geteuid() == 0)
 
 
-def run_setup():
+def run_setup(has_sudo):
     print('Setting up idle notifier...')
 
     install_path = os.path.dirname(os.path.realpath(__file__))
@@ -28,8 +37,8 @@ def run_setup():
     script_path = os.path.join(install_path, 'main.py')
     print(f'Will use the following python interpreter: {python_path}.')
 
-    api_key = input('Please enter SimplePush API key:')
-    device_name = input('Please enter device name:')
+    api_key = input('Please enter SimplePush API key: ')
+    device_name = input('Please enter device name: ')
     with open(os.path.join(install_path, '_config.json')) as fid:
         cfg = json.load(fid)
         cfg['key'] = api_key
@@ -37,41 +46,49 @@ def run_setup():
     with open(os.path.join(install_path, 'config.json'), 'w') as fid:
         json.dump(cfg, fid, indent=4)
 
-    with open(os.path.join(install_path, 'idle_notifier.service'), 'r') as fid:
+    with open(os.path.join(install_path, '_idle_notifier.service'), 'r') as fid:
         service_str = fid.read()
     service_str = service_str.replace('{PYTHON_PATH}', python_path)
     service_str = service_str.replace('{SCRIPT_PATH}', script_path)
 
-    dst_path = '/etc/systemd/system/idle_notifier.service'
+    dst_path = os.path.join(install_path, 'idle_notifier.service')
     with open(dst_path, 'w') as fid:
         fid.write(service_str)
-    os.chmod(dst_path, 744)
 
-    print(f'Installation complete. Written service to {dst_path}')
-    print("Run 'sudo systemctl enable idle_notifier' to enable service upon boot")
+    systemd_path = os.path.join('/etc/systemd/system/', 'idle_notifier.service')
 
-
-def run_uninstall():
-    dst_path = '/etc/systemd/system/idle_notifier.service'
-    if os.path.exist(dst_path):
-        os.remove(dst_path)
-        print(f'Removed {dst_path}')
-        subprocess.call(['systemctl', 'disable', 'idle_notifier'])
-        print('Uninstalled idle_notifier')
+    if has_sudo:
+        os.chmod(dst_path, 0o777)
+        os.symlink(dst_path, systemd_path)
+        subprocess.call(['systemctl', 'enable', 'idle_notifier'])
+        subprocess.call(['systemctl', 'start', 'idle_notifier'])
+        print(f'Installation complete.')
     else:
-        print('Not installed')
+        print('Config file written. Note that installation is not complete. Run the following commands '
+              'to write to the systemd folder to start the service and enable startup on bot\n')
+        print(f"  sudo ln -s {dst_path} {systemd_path}")
+        print( "  sudo systemctl enable idle_notifier")
+        print( "  sudo systemctl start idle_notifier")
 
+
+def run_uninstall(has_sudo):
+    systemd_path = '/etc/systemd/system/idle_notifier.service'
+    if os.path.exists(systemd_path):
+        if has_sudo:
+            subprocess.call(['systemctl', 'stop', 'idle_notifier'])
+            subprocess.call(['systemctl', 'disable', 'idle_notifier'])
+            print('Uninstalled idle_notifier')
+
+        else:
+            print('Follow the following instructions to uninstall\n')
+            print('sudo systemctl stop idle_notifier')
+            print('sudo systemctl disable idle_notifier')
+    else:
+        print('idle_notifier was not installed. Not proceeding with uninstallation.')
+
+
+def idle_notifier_setup():
+    setup_wrapper('install')
 
 def idle_notifier_remove():
-    if os.geteuid() == 0:
-        run_uninstall()
-    else:
-        python_path = sys.executable
-        result = input('You need sudo to run this setup script. Run with sudo rights? [y/n]')
-        while result not in ['y', 'n']:
-            result = input('Invalid response. Run setup script with sudo? [y/n]')
-        if result == 'y':
-            subprocess.call(['sudo', python_path, *sys.argv])
-            sys.exit()
-        else:
-            print('Terminating...')
+    setup_wrapper('uninstall')
